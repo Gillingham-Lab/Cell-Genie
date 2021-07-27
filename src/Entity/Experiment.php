c31ac98506c45bfe655d9aa9dfe632a71df33c4d
Basilius Sauter <basilius.sauter@gmail.com>
2021-07-27 12:40:49 +0200

    Experiment entity allows not ID to be optional int and is null by default.

--- src/Entity/Experiment.php
+++ src/Entity/Experiment.php
@@ -22,7 +22,7 @@ class Experiment
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
      */
-    private int $id;
+    private ?int $id = null;
 
     /**
      * @ORM\Column(type="string", length=255)
