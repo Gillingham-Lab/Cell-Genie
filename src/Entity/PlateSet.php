e4c401194c6981ebd421db42c07a9745ae5e2a32
Basilius Sauter <basilius.sauter@gmail.com>
2022-09-05 15:46:07 +0200

    Added Loggable to PlateSet entity.

--- src/Entity/PlateSet.php
+++ src/Entity/PlateSet.php
@@ -8,8 +8,10 @@ use App\Repository\PlateSetRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
 
 #[ORM\Entity(repositoryClass: PlateSetRepository::class)]
+#[Gedmo\Loggable]
 class PlateSet
 {
     use IdTrait;
