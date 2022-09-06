3f8a4c0a2d5c29a3b3334ff857ee1ac21213829b
Basilius Sauter <basilius.sauter@gmail.com>
2022-09-06 08:48:41 +0200

    Adds twig tests for substances.

---
+++ src/Twig/SubstanceTest.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Twig;
+
+use App\Entity\DoctrineEntity\Substance\Antibody;
+use App\Entity\DoctrineEntity\Substance\Chemical;
+use App\Entity\DoctrineEntity\Substance\Protein;
+use App\Entity\DoctrineEntity\Substance\Substance;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigTest;
+
+class SubstanceTest extends AbstractExtension
+{
+    function getTests(): array
+    {
+        return [
+            new TwigTest("substance", fn(?object $substance) => $substance instanceof Substance),
+            new TwigTest("substanceAntibody", fn(?object $substance) => $substance instanceof Antibody),
+            new TwigTest("substanceChemical", fn(?object $substance) => $substance instanceof Chemical),
+            new TwigTest("substanceProtein", fn(?object $substance) => $substance instanceof Protein),
+        ];
+    }
+}
