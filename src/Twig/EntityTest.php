45b3349fbead541f121ae132c26e727e5e7b4e79
Basilius Sauter <basilius.sauter@gmail.com>
2022-10-12 07:48:57 +0200

    Slightly improves interface; updates custom icons; adds icons to cell/substance views.

---
+++ src/Twig/EntityTest.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Twig;
+
+use App\Entity\DoctrineEntity\Cell\Cell;
+use App\Entity\DoctrineEntity\Cell\CellCultureEvent;
+use App\Entity\DoctrineEntity\Cell\CellCultureOtherEvent;
+use App\Entity\DoctrineEntity\Cell\CellCultureSplittingEvent;
+use App\Entity\DoctrineEntity\Cell\CellCultureTestEvent;
+use App\Entity\DoctrineEntity\Substance\Antibody;
+use App\Entity\DoctrineEntity\Substance\Chemical;
+use App\Entity\DoctrineEntity\Substance\Oligo;
+use App\Entity\DoctrineEntity\Substance\Protein;
+use App\Entity\DoctrineEntity\Substance\Substance;
+use App\Entity\Epitope;
+use App\Entity\Lot;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigTest;
+
+class EntityTest extends AbstractExtension
+{
+    function getTests(): array
+    {
+        return [
+            new TwigTest("integer", fn(mixed $object) => is_int($object)),
+
+            new TwigTest("cell", fn(?object $substance) => $substance instanceof Cell),
+
+            new TwigTest("cellCultureEvent", fn(?object $object) => $object instanceof CellCultureEvent),
+            new TwigTest("cellCultureOtherEvent", fn(?object $object) => $object instanceof CellCultureOtherEvent),
+            new TwigTest("cellCultureTestEvent", fn(?object $object) => $object instanceof CellCultureTestEvent),
+            new TwigTest("cellCultureSplittingEvent", fn(?object $object) => $object instanceof CellCultureSplittingEvent),
+
+            new TwigTest("substance", fn(?object $substance) => $substance instanceof Substance),
+            new TwigTest("substanceAntibody", fn(?object $substance) => $substance instanceof Antibody),
+            new TwigTest("substanceChemical", fn(?object $substance) => $substance instanceof Chemical),
+            new TwigTest("substanceProtein", fn(?object $substance) => $substance instanceof Protein),
+            new TwigTest("substanceOligo", fn(?object $substance) => $substance instanceof Oligo),
+
+            new TwigTest("lot", fn(?object $lot) => $lot instanceof Lot),
+
+            new TwigTest("epitope", fn(?object $epitope) => $epitope instanceof Epitope),
+        ];
+    }
+}
