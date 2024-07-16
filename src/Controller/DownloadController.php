e5b4d6094659829c33551895ff917d89a6c6dc10
Vassyli <basilius.sauter@gmail.com>
2024-07-16 08:30:58 +0200

    Replaces all Annotation\Route with Attribute\Route

--- src/Controller/DownloadController.php
+++ src/Controller/DownloadController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\File\Exception\FileNotFoundException;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\HttpKernel\Attribute\Cache;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class DownloadController extends AbstractController
 {
