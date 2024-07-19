0fd1e837020d8c6a45f4eb01c41b9c8eac7fd0d8
Vassyli <basilius.sauter@gmail.com>
2024-07-19 10:15:17 +0200

    Improves validation of ExperimentalDesigns.

--- src/Validator/Constraint/UniqueCollectionField.php
+++ src/Validator/Constraint/UniqueCollectionField.php
@@ -7,18 +7,24 @@ use App\Validator\UniqueCollectionFieldValidator;
 use Attribute;
 use Symfony\Component\Validator\Constraint;
 
-#[Attribute(Attribute::TARGET_PROPERTY)]
+#[Attribute(Attribute::TARGET_PROPERTY | Attribute::IS_REPEATABLE)]
 class UniqueCollectionField extends Constraint
 {
     public string $message = 'The field {field} must be unique.';
-    public string $localMessage = 'This field is duplicated.';
+    public string $localMessage = 'This field must be unique, but appears here a second time.';
 
     public function __construct(
         public readonly string $field,
+        public readonly ?string $errorPath = null,
+        ?string $message = null,
+        ?string $localMessage = null,
         ?array $groups = null,
         mixed $payload = null
     ) {
         parent::__construct([], $groups, $payload);
+
+        $this->message = $message ?? $this->message;
+        $this->localMessage = $localMessage ?? $this->localMessage;
 
         $this->message = str_replace("{field}", $this->field, $this->message);
     }
